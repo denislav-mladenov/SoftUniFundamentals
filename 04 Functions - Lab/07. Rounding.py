3a4a19b734d73c3777d67de49919831cd0f12bc0
denislav-mladenov <denislav.mladenov.89@gmail.com>
2023-03-30 09:45:37 +0300

    Lists Basics - Lab exercise 07

---
+++ 04 Functions - Lab/07. Rounding.py
@@ -0,0 +1,12 @@
+numbers = input()
+number_list = numbers.split(" ")
+absolute_values = [round(float(number)) for number in number_list]
+print(absolute_values)
+
+# numbers = input().split()
+# listt = []
+#
+# for i in numbers:
+#     listt.append(abs(float(i)))
+#
+# print(listt)
