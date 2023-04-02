3a31d404d8ec2c5bca1b33f4eed11de0685e4759
denislav-mladenov <denislav.mladenov.89@gmail.com>
2023-04-02 13:30:53 +0300

    Lists Basics - homework exercise 07

---
+++ 04 Functions - Work/07. Min Max and Sum.py
@@ -0,0 +1,7 @@
+nums = [int(x) for x in input().split()]
+min_nums = min(nums)
+max_nums = max(nums)
+sum_nums = sum(nums)
+print(f"The minimum number is {min_nums}")
+print(f"The maximum number is {max_nums}")
+print(f"The sum number is: {sum_nums}")
